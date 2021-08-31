22e01f0f4ac88837d1055eec6aa268335e31627a
kruno <petrik.kpetrik@gmail.com>
2021-09-01 00:17:30 +0200

    Added traffic lights xml cascade

--- run.py
+++ run.py
@@ -5,14 +5,15 @@ import functions
 
 cap = cv.VideoCapture(0)
 stop_cascade = cv.CascadeClassifier(os.path.join(os.getcwd(), "haarcascades/stop_sign.xml"))
-
+traffic_cascade = cv.CascadeClassifier(os.path.join(os.getcwd(), "haarcascades/traffic_light.xml"))
 
 while True:
     ret, frame = cap.read()
     if frame is None:
         print('--(!) No captured frame -- Break!')
         break
-    num_of_detected_stop_signs = functions.detectAndDisplayStopSigns(frame, stop_cascade)
-    print(num_of_detected_stop_signs)
+    num_of_detected_stop_signs = functions.detectAndDisplay(frame, stop_cascade)
+    num_of_detected_traffic_lights = functions.detectAndDisplay(frame, traffic_cascade)
+    print(str(num_of_detected_stop_signs)+", "+str(num_of_detected_traffic_lights))
     if cv.waitKey(10) == 27:
         break
