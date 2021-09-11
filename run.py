6433c7d833fbde827b0df3c72e50e98b1326322e
kruno <petrik.kpetrik@gmail.com>
2021-09-11 17:43:09 +0200

    Changed capture to pi camera

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import cv2 as cv
-import argparse
 import os
 import functions
 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture("http://192.168.1.11:8080/?action=stream")
 stop_cascade = cv.CascadeClassifier(os.path.join(os.getcwd(), "haarcascades/stop_sign.xml"))
 traffic_cascade = cv.CascadeClassifier(os.path.join(os.getcwd(), "haarcascades/traffic_light.xml"))
 
@@ -12,7 +11,6 @@ while True:
     if frame is None:
         print('--(!) No captured frame -- Break!')
         break
-
     stop_signs = functions.detectAndDisplay(frame, stop_cascade)
     traffic_lights = functions.detectAndDisplay(frame, traffic_cascade)
 
